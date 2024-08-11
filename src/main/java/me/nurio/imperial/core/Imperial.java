a43526940a121ff98246a3e8974d24dcb33b1ebb
Nurio Fernández <n.fernandez@efg.gg>
2024-08-11 18:35:48 +0200

    Added dependency OrganizationFactory to Imperial main

--- src/main/java/me/nurio/imperial/core/Imperial.java
+++ src/main/java/me/nurio/imperial/core/Imperial.java
@@ -1,18 +1,22 @@
 package me.nurio.imperial.core;
 
 import lombok.Getter;
+import me.nurio.imperial.core.organizations.OrganizationFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Imperial extends JavaPlugin {
 
-    @Getter
-    private static Plugin plugin;
+    @Getter private static Plugin plugin;
+    @Getter private static OrganizationFactory organizationFactory;
 
     @Override
     public void onEnable() {
         plugin = this;
+
+        // Register Organization Factory
+        organizationFactory = new OrganizationFactory();
 
         // Send loaded message
         Bukkit.getLogger().info("Imperial is installed!");

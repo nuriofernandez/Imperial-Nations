aa8ed8d7ea603974c803b22a377e7e732f5e4b72
Nurio Fernández <n.fernandez@efg.gg>
2024-08-11 18:19:38 +0200

    Added static Imperial.getPlugin()

--- src/main/java/me/nurio/imperial/core/Imperial.java
+++ src/main/java/me/nurio/imperial/core/Imperial.java
@@ -1,12 +1,19 @@
 package me.nurio.imperial.core;
 
+import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Imperial extends JavaPlugin {
 
+    @Getter
+    private static Plugin plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
+
         // Send loaded message
         Bukkit.getLogger().info("Imperial is installed!");
     }

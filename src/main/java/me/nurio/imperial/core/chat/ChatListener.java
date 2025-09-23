6cc59fa9c2d4aa95fbfbfe22ba5202bc8d066750
Nurio Fernandez <personal@nurio.me>
2025-09-23 05:00:38 +0200

    Fixed message chat serializer

--- src/main/java/me/nurio/imperial/core/chat/ChatListener.java
+++ src/main/java/me/nurio/imperial/core/chat/ChatListener.java
@@ -1,6 +1,7 @@
 package me.nurio.imperial.core.chat;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,8 +16,9 @@ public class ChatListener implements Listener {
         MessageSendAttempt attempt = new MessageSendAttempt(eve.getPlayer(), eve.message());
         Bukkit.getOnlinePlayers().forEach(attempt::send);
 
+        String plainMessage = PlainTextComponentSerializer.plainText().serialize(eve.message());
         Bukkit.getLogger().info(
-                String.format("%s : '%s'", eve.getPlayer().getName(), eve.message())
+                String.format("%s : '%s'", eve.getPlayer().getName(), plainMessage)
         );
     }
 

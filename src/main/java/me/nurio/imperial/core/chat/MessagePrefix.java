2e95b11cc7dbdca9b0853d33121fdb52dc9fbcb5
Nurio Fernandez <personal@nurio.me>
2025-09-23 12:56:46 +0200

    Added time played to chat

--- src/main/java/me/nurio/imperial/core/chat/MessagePrefix.java
+++ src/main/java/me/nurio/imperial/core/chat/MessagePrefix.java
@@ -1,7 +1,9 @@
 package me.nurio.imperial.core.chat;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 
 public class MessagePrefix {
@@ -11,9 +13,18 @@ public class MessagePrefix {
     public static Component messagePrefix(Player sender) {
         return OrganizationPrefix.getComponent(sender)
             .appendSpace()
+            .append(Component.text(String.format(
+                    "(☀%.2f)", playedDays(sender)
+            )).color(NamedTextColor.GREEN))
+            .appendSpace()
             .append(sender.displayName())
             .appendSpace()
             .append(suffix);
+    }
+
+    private static double playedDays(Player sender) {
+        double time = sender.getStatistic(Statistic.PLAY_ONE_MINUTE);
+        return time / 20 * 60 * 60 * 24; // day in ticks
     }
 
 }
